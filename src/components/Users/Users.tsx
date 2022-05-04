e4eb3cf73d93647e38a7fb0f2939a865692681c6
Sergei <rudnev09@mail.ru>
2022-05-04 20:46:59 +0300

    54 - React JS практика - жизненный цикл, life cycle methods, componentDidMount

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -5,8 +5,14 @@ import axios from "axios";
 
 export class Users extends React.Component<UsersPropsType, UsersPropsType> {
 
-  constructor(props: UsersPropsType) {
-    super(props);
+  // constructor(props: UsersPropsType) {
+  //   super(props);
+  //   axios.get('https://social-network.samuraijs.com/api/1.0/users/').then(response => {
+  //     this.props.setUsers(response.data.items)
+  //   })
+  // }
+
+  componentDidMount() {
     axios.get('https://social-network.samuraijs.com/api/1.0/users/').then(response => {
       this.props.setUsers(response.data.items)
     })
@@ -52,51 +58,3 @@ export class Users extends React.Component<UsersPropsType, UsersPropsType> {
     );
   }
 }
-
-// export const Users: React.FC<UsersPropsType> = (props) => {
-//
-//   const avatar = 'https://avatars.mds.yandex.net/i?id=c6aff9195dcb3b2ece8089da6e422a15-5528343-images-thumbs&ref=rim&n=33&w=150&h=150'
-//
-//   const follow = (userId: number) => {
-//     props.follow(userId)
-//   }
-//
-//   const unfollow = (userId: number) => {
-//     props.unfollow(userId)
-//   }
-//
-//   const getUsers = () => {
-//     if(props.users.length === 0) {
-//       axios.get('https://social-network.samuraijs.com/api/1.0/users/').then(response => {
-//         props.setUsers(response.data.items)
-//       })
-//     }
-//   }
-//
-//   return (
-//     <div className={s.users}>
-//       <button onClick={getUsers}>GetUsers</button>
-//       {props.users.map(user => {
-//         return <div key={user.id} className={s.user}>
-//           <div className={s.userAvatar}>
-//             <img alt={'avatar'} src={user.photos.small ? user.photos.small : avatar}/>
-//             {user.followed
-//               ? <button onClick={() => unfollow(user.id)}>unfollow</button>
-//               : <button onClick={() => follow(user.id)}>follow</button>}
-//           </div>
-//           <div className={s.userData}>
-//             <div>
-//               <div>{user.name}</div>
-//               <div>{user.status}</div>
-//             </div>
-//             {/*<div>*/}
-//             {/*  <div>{user.location.city}</div>*/}
-//             {/*  <div>{user.location.country}</div>*/}
-//             {/*</div>*/}
-//           </div>
-//         </div>
-//       })}
-//       <button onClick={getUsers}>show more</button>
-//     </div>
-//   );
-// };
